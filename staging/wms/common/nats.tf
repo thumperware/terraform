3b7538d0217aab6f6b22c3f4297ca1c79f6de7e1
Hooman Familrouhani <hooman.fr@gmail.com>
2023-11-19 21:19:54 -0800

    nats on k8

--- staging/wms/common/nats.tf
+++ staging/wms/common/nats.tf
@@ -1,4 +1,4 @@
-resource "helm_resource" "nats_server"{
+resource "helm_release" "nats_server"{
     name = "nats-server"
     repository = "https://nats-io.github.io/k8s/helm/charts/"
     chart = "nats/nats"
