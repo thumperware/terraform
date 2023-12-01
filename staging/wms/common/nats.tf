adc5db764c05a236fb1ec438b0db36d168a84975
Hooman Familrouhani <hooman.fr@gmail.com>
2023-11-30 20:32:43 -0800

    nats with jestream and cluster

--- staging/wms/common/nats.tf
+++ staging/wms/common/nats.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "nats_server"{
     name = "nats-server"
     repository = "https://nats-io.github.io/k8s/helm/charts/"
-    chart = "nats"
+    chart = "nats/nats"
     namespace = "default"
 }
