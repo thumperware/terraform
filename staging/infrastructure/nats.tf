7b72f0d14875efdecfc39bfe8b6b982f96007f0f
hooman familrouhani <hoomanfr@hoomans-MacBook-Pro.local>
2024-03-01 14:34:32 -0800

    setting up istio

--- staging/infrastructure/nats.tf
+++ staging/infrastructure/nats.tf
@@ -1,5 +1,5 @@
 resource "helm_release" "nats_server"{
-    provider = kubernetes.central
+    provider = helm.central
     name = "nats-server"
     repository = "https://nats-io.github.io/k8s/helm/charts/"
     chart = "nats"
