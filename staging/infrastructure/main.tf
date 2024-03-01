d09c986e242d5699addf1e472e0c0df66756f05d
hooman familrouhani <hoomanfr@hoomans-MacBook-Pro.local>
2024-02-29 19:03:54 -0800

    enabling apis

--- staging/infrastructure/main.tf
+++ staging/infrastructure/main.tf
@@ -12,18 +12,3 @@ provider "helm" {
     token                  = data.google_client_config.current.access_token
   }
 }
-
-resource "google_project_service" "gcp_apis" {
-  for_each = toset(["storage.googleapis.com",
-    "artifactregistry.googleapis.com",
-    "compute.googleapis.com",
-    "container.googleapis.com",
-    "cloudresourcemanager.googleapis.com",
-    "servicenetworking.googleapis.com",
-    "sqladmin.googleapis.com"])
-
-  project = var.project_id
-  service = each.key
-
-  disable_dependent_services = true
-}
--- staging/terraform/main.tf
+++ staging/terraform/main.tf
@@ -1,4 +1,3 @@
-# Terraform
 provider "google" {
   project = var.project_id
   region  = var.region
@@ -8,8 +7,12 @@ resource "google_project_service" "gcp_apis" {
   for_each = toset([
     "cloudresourcemanager.googleapis.com",
     "storage.googleapis.com",
+    "artifactregistry.googleapis.com",
+    "servicenetworking.googleapis.com",
     "compute.googleapis.com",
-    "cloudkms.googleapis.com"])
+    "cloudkms.googleapis.com",
+    "container.googleapis.com",
+    "sqladmin.googleapis.com"])
 
   project = var.project_id
   service = each.key
