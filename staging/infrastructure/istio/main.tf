f12cb1516007fdc652a367de42cc12a63ec49cf5
hooman familrouhani <hoomanfr@hoomans-MacBook-Pro.local>
2024-03-04 17:27:07 -0800

    istio

--- staging/infrastructure/istio/main.tf
+++ staging/infrastructure/istio/main.tf
@@ -5,11 +5,16 @@ provider "google" {
 
 data "google_client_config" "current" {}
 
+data "google_container_cluster" "primary" {
+  name     = "${var.project_id}-gke"
+  location = var.region
+}
+
 provider "helm" {
   alias = "central"
   kubernetes {
-    host                   = var.k8s_cluster_endpoint
-    cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+    host                   = data.google_container_cluster.primary.endpoint
+    cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
     token                  = data.google_client_config.current.access_token
   }
 }
