f6673b759b232df39cfde639bfc12164ce45751a
hooman familrouhani <hoomanfr@hoomans-MacBook-Pro.local>
2024-03-08 23:30:07 -0800

    dropped unused ingress while switching to istio

--- staging/infrastructure/istio/istio.tf
+++ staging/infrastructure/istio/istio.tf
@@ -2,55 +2,55 @@ locals {
   istio_charts_url = "https://istio-release.storage.googleapis.com/charts"
 }
 
-resource "kubernetes_namespace" "istio-ingress" {
-  provider = kubernetes.central
-  metadata {
-    name = "istio-ingress"
-    labels = {
-      istio-injection = "enabled"
-      istio-discovery = "enabled"
-    }
-  }
-}
+# resource "kubernetes_namespace" "istio-ingress" {
+#   provider = kubernetes.central
+#   metadata {
+#     name = "istio-ingress"
+#     labels = {
+#       istio-injection = "enabled"
+#       istio-discovery = "enabled"
+#     }
+#   }
+# }
 
-resource "helm_release" "istio-base" {
-  provider         = helm.central
-  repository       = local.istio_charts_url
-  chart            = "base"
-  name             = "istio-base"
-  namespace        = "istio-system"
-  version          = "1.20.2"
-  create_namespace = true
-}
+# resource "helm_release" "istio-base" {
+#   provider         = helm.central
+#   repository       = local.istio_charts_url
+#   chart            = "base"
+#   name             = "istio-base"
+#   namespace        = "istio-system"
+#   version          = "1.20.2"
+#   create_namespace = true
+# }
 
-resource "helm_release" "istiod" {
-  provider         = helm.central
-  repository       = local.istio_charts_url
-  chart            = "istiod"
-  name             = "istiod"
-  namespace        = "istio-system"
-  create_namespace = true
-  version          = "1.20.2"
-  depends_on       = [helm_release.istio-base]
-}
+# resource "helm_release" "istiod" {
+#   provider         = helm.central
+#   repository       = local.istio_charts_url
+#   chart            = "istiod"
+#   name             = "istiod"
+#   namespace        = "istio-system"
+#   create_namespace = true
+#   version          = "1.20.2"
+#   depends_on       = [helm_release.istio-base]
+# }
 
-resource "google_compute_global_address" "istio-ingress-ipv4" {
-  name       = "istio-gke-ingress-ipv4"
-  ip_version = "IPV4"
-}
+# resource "google_compute_global_address" "istio-ingress-ipv4" {
+#   name       = "istio-gke-ingress-ipv4"
+#   ip_version = "IPV4"
+# }
 
-resource "helm_release" "istio-ingress" {
-  provider   = helm.central
-  repository = local.istio_charts_url
-  chart      = "gateway"
-  name       = "istio-ingress"
-  namespace  = kubernetes_namespace.istio-ingress.metadata.0.name
-  version    = "1.20.2"
-  depends_on = [helm_release.istiod, google_compute_global_address.istio-ingress-ipv4]
-  values = [templatefile("values.yaml", {
-    ip = google_compute_global_address.istio-ingress-ipv4.address
-  })]
-}
+# resource "helm_release" "istio-ingress" {
+#   provider   = helm.central
+#   repository = local.istio_charts_url
+#   chart      = "gateway"
+#   name       = "istio-ingress"
+#   namespace  = kubernetes_namespace.istio-ingress.metadata.0.name
+#   version    = "1.20.2"
+#   depends_on = [helm_release.istiod, google_compute_global_address.istio-ingress-ipv4]
+#   values = [templatefile("values.yaml", {
+#     ip = google_compute_global_address.istio-ingress-ipv4.address
+#   })]
+# }
 
 # resource "null_resource" "istio-load-balancer-ip-patch" {
 #   provisioner "local-exec" {
