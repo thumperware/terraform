58de4e358e84f444e76ab6d35c76221fa68e2080
hooman familrouhani <hoomanfr@hoomans-MacBook-Pro.local>
2024-03-08 23:20:13 -0800

    dropped unused ingress while switching to istio

--- staging/infrastructure/istio/istio.tf
+++ staging/infrastructure/istio/istio.tf
@@ -47,36 +47,37 @@ resource "helm_release" "istio-ingress" {
   namespace  = kubernetes_namespace.istio-ingress.metadata.0.name
   version    = "1.20.2"
   depends_on = [helm_release.istiod, google_compute_global_address.istio-ingress-ipv4]
+  values = [ "service: loadBalancerIP: ${google_compute_global_address.istio-ingress-ipv4.address}" ]
 }
 
-resource "null_resource" "istio-load-balancer-ip-patch" {
-  provisioner "local-exec" {
-    command = <<EOH
-cat >/tmp/ca.crt <<EOF
-${base64decode(data.google_container_cluster.primary.master_auth.0.cluster_ca_certificate)}
-EOF
-  kubectl \
-  --server="https://${data.google_container_cluster.primary.endpoint}" \
-  --token="${data.google_client_config.current.access_token}" \
-  --certificate_authority=/tmp/ca.crt \
-  patch service istio-ingress --patch '{"spec":{"loadBalancerIP": "${google_compute_global_address.istio-ingress-ipv4.address}"}, "status":{"loadBalancer":{"ingress":[{"ip":"${google_compute_global_address.istio-ingress-ipv4.address}"}]}}}' --namespace istio-ingress
-EOH
-  }
-  depends_on = [ helm_release.istio-ingress ]
-}
+# resource "null_resource" "istio-load-balancer-ip-patch" {
+#   provisioner "local-exec" {
+#     command = <<EOH
+# cat >/tmp/ca.crt <<EOF
+# ${base64decode(data.google_container_cluster.primary.master_auth.0.cluster_ca_certificate)}
+# EOF
+#   kubectl \
+#   --server="https://${data.google_container_cluster.primary.endpoint}" \
+#   --token="${data.google_client_config.current.access_token}" \
+#   --certificate_authority=/tmp/ca.crt \
+#   patch service istio-ingress --patch '{"spec":{"loadBalancerIP": "${google_compute_global_address.istio-ingress-ipv4.address}"}, "status":{"loadBalancer":{"ingress":[{"ip":"${google_compute_global_address.istio-ingress-ipv4.address}"}]}}}' --namespace istio-ingress
+# EOH
+#   }
+#   depends_on = [ helm_release.istio-ingress ]
+# }
 
-resource "null_resource" "istio-load-ingress-loadbalancer-ip-patch" {
-  provisioner "local-exec" {
-    command = <<EOH
-cat >/tmp/ca.crt <<EOF
-${base64decode(data.google_container_cluster.primary.master_auth.0.cluster_ca_certificate)}
-EOF
-  kubectl \
-  --server="https://${data.google_container_cluster.primary.endpoint}" \
-  --token="${data.google_client_config.current.access_token}" \
-  --certificate_authority=/tmp/ca.crt \
-  patch service istio-ingress --patch '{"status":{"loadBalancer":{"ingress":[{"ip": "${google_compute_global_address.istio-ingress-ipv4.address}" }]}}}' --namespace istio-ingress
-EOH
-  }
-  depends_on = [ helm_release.istio-ingress, null_resource.istio-load-balancer-ip-patch ]
-}
+# resource "null_resource" "istio-load-ingress-loadbalancer-ip-patch" {
+#   provisioner "local-exec" {
+#     command = <<EOH
+# cat >/tmp/ca.crt <<EOF
+# ${base64decode(data.google_container_cluster.primary.master_auth.0.cluster_ca_certificate)}
+# EOF
+#   kubectl \
+#   --server="https://${data.google_container_cluster.primary.endpoint}" \
+#   --token="${data.google_client_config.current.access_token}" \
+#   --certificate_authority=/tmp/ca.crt \
+#   patch service istio-ingress --patch '{"status":{"loadBalancer":{"ingress":[{"ip": "${google_compute_global_address.istio-ingress-ipv4.address}" }]}}}' --namespace istio-ingress
+# EOH
+#   }
+#   depends_on = [ helm_release.istio-ingress, null_resource.istio-load-balancer-ip-patch ]
+# }
