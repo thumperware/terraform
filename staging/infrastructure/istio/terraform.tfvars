e203d2ff613c5ff57576cf87c9a4816082450888
hooman familrouhani <hoomanfr@hoomans-MacBook-Pro.local>
2024-03-08 23:46:38 -0800

    dropped unused ingress while switching to istio

--- staging/infrastructure/istio/terraform.tfvars
+++ staging/infrastructure/istio/terraform.tfvars
@@ -1,3 +1,2 @@
 project_id = "thumperq-416219"
 region  = "us-west1"
-k8s_cluster_endpoint = "https://10.10.0.30"
--- staging/infrastructure/istio/variables.tf
+++ staging/infrastructure/istio/variables.tf
@@ -7,8 +7,3 @@ variable "region" {
   type        = string
   description = "The Region"
 }
-
-variable "k8s_cluster_endpoint" {
-  type        = string
-  description = "The k8s cluster endpoint"
-}
