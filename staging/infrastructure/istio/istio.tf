7d5270f7fbbbca4abb2c67c65edcc2df9847bbee
hooman familrouhani <hoomanfr@hoomans-MacBook-Pro.local>
2024-03-08 21:28:30 -0800

    dropped unused ingress while switching to istio

--- staging/infrastructure/istio/istio.tf
+++ staging/infrastructure/istio/istio.tf
@@ -43,3 +43,22 @@ resource "helm_release" "istio-ingress" {
   version    = "1.20.2"
   depends_on = [helm_release.istiod]
 }
+
+resource "google_compute_global_address" "istio-ingress-ipv4" {
+  name       = "istio-gke-ingress-ipv4"
+  ip_version = "IPV4"
+}
+
+resource "kubernetes_patch" "istio-ingress-static-ip" {
+  kind       = "Service"
+  api_version = "v1"
+  name       = "istio-ingress"
+  namespace  = kubernetes_namespace.istio-ingress.metadata.0.name
+
+  patch = jsonencode({
+    spec = {
+      loadBalancerIP = google_compute_global_address.istio-ingress-ipv4.address
+    }
+  })
+  depends_on = [ helm_release.istio-ingress, google_compute_global_address.istio-ingress-ipv4 ]
+}
--- staging/wms/shared/lb.tf
+++ staging/wms/shared/lb.tf
@@ -1,5 +1,5 @@
-# Static IPv4 address for Ingress Load Balancing
-resource "google_compute_global_address" "ingress-ipv4" {
-  name       = "${var.service_name}-gke-ingress-ipv4"
-  ip_version = "IPV4"
-}
+# # Static IPv4 address for Ingress Load Balancing
+# resource "google_compute_global_address" "ingress-ipv4" {
+#   name       = "${var.service_name}-gke-ingress-ipv4"
+#   ip_version = "IPV4"
+# }
